8c1084880b54b7459b1db32ae038809dfdfd8b4f
Al Rodriguez <ProgrammerAl@outlook.com>
2024-04-27 18:01:54 -0400

    Got the UI looking okay and functional

--- src/SvgMover/SvgMover/Pages/Index.razor.cs
+++ src/SvgMover/SvgMover/Pages/Index.razor.cs
@@ -5,21 +5,120 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 
+using BlazorMonaco.Editor;
+
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace ProgrammerAl.SvgMover.Pages;
 
 public partial class Index : ComponentBase
 {
-    private string OriginalText { get; set; } = "";
-    private string ModifiedText { get; set; } = "";
+    //private StandaloneCodeEditor _originalEditor = null!;
+    //private StandaloneCodeEditor _mondifiedEditor = null!;
 
-    private void MoveSvg()
+    private const string EditorOriginalTextModel = "editor-original-text-model";
+    private const string EditorModifiedTextModel = "editor-modified-text-model";
+
+    private const string DefaultSvgText =
+"""
+<svg xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+</svg>
+""";
+
+
+    private StandaloneDiffEditor _diffEditor = null!;
+
+    [Inject, NotNull]
+    private IJSRuntime? JsRuntime { get; set; }
+
+    private string XAmount { get; set; } = "0";
+    private string YAmount { get; set; } = "0";
+
+    private StandaloneDiffEditorConstructionOptions DiffEditorConstructionOptions(StandaloneDiffEditor editor)
     {
-        ModifiedText = string.Empty;
-        StateHasChanged();
+        return new StandaloneDiffEditorConstructionOptions
+        {
+            OriginalEditable = true,
+        };
+    }
 
-        ModifiedText = "Some Test";
+    private async Task EditorOnDidInit()
+    {
+        var originalModel = await GetOrCreateTextModelAsync(EditorOriginalTextModel);
+        var modifiedModel = await GetOrCreateTextModelAsync(EditorModifiedTextModel);
+
+        await _diffEditor.SetModel(new DiffEditorModel
+        {
+            Original = originalModel,
+            Modified = modifiedModel
+        });
+    }
+
+    private async Task<TextModel> GetOrCreateTextModelAsync(string textModelName)
+    {
+        var textValue = await BlazorMonaco.Editor.Global.GetModel(JsRuntime, textModelName);
+        if (textValue is null)
+        {
+            textValue = await BlazorMonaco.Editor.Global.CreateModel(JsRuntime, DefaultSvgText, "javascript", textModelName);
+        }
+
+        return textValue;
+    }
+
+    private StandaloneEditorConstructionOptions EditorConstructionOptions(StandaloneCodeEditor editor)
+    {
+        return new StandaloneEditorConstructionOptions
+        {
+            AutomaticLayout = true,
+            //InDiffEditor = true,
+            //OriginalEditable = true,
+            Language = "xml",
+            GlyphMargin = true,
+            FormatOnPaste = false,
+            Value =
+"""
+<svg xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+</svg>
+"""
+        };
+    }
+
+    private async Task MoveSvgAsync()
+    {
+        var originalModel = await GetOrCreateTextModelAsync(EditorOriginalTextModel);
+        var modifiedModel = await GetOrCreateTextModelAsync(EditorModifiedTextModel);
+
+        var originalText = await originalModel.GetValue(eol: null, preserveBOM: null);
+        var modifiedText = MoveSvgElements(originalText);
+
+        await modifiedModel.SetValue(modifiedText);
+
+        await _diffEditor.SetModel(new DiffEditorModel
+        {
+            Original = originalModel,
+            Modified = modifiedModel
+        });
+
         StateHasChanged();
+    }
+
+    private string MoveSvgElements(string svgText)
+    {
+        if (!int.TryParse(XAmount, out int xAmount))
+        {
+            xAmount = 0;
+        }
+
+        if (!int.TryParse(YAmount, out int yAmount))
+        {
+            yAmount = 0;
+        }
+
+        
+
+        return svgText;
     }
 }
