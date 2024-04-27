2d33f65349dbacb6e25598752b052a9fd4e18381
Al Rodriguez <ProgrammerAl@outlook.com>
2024-04-27 12:53:55 -0400

    Initial messing with main ui

---
+++ src/SvgMover/SvgMover/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Components;
+
+namespace ProgrammerAl.SvgMover.Pages;
+
+public partial class Index : ComponentBase
+{
+    private string OriginalText { get; set; } = "";
+    private string ModifiedText { get; set; } = "";
+
+    private void MoveSvg()
+    {
+        ModifiedText = string.Empty;
+        StateHasChanged();
+
+        ModifiedText = "Some Test";
+        StateHasChanged();
+    }
+}
